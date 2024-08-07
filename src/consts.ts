6a4d5236ae2a1505def6355933881afb1138a6f3
Mowang <mw@loli.wang>
2024-08-07 17:53:14 +0800

    🎉 init:

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = '魔王 の WEB工具箱';
+export const SITE_DESCRIPTION = '魔王 の WEB工具箱';
