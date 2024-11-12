ca7e6b149ab062dec1bba918e90b3ad30ac2b699
Mowang <mw@loli.wang>
2024-11-12 11:04:09 +0800

    ✨ feat: 增加二维码生成

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tools.loli.wang', 
+  site: 'https://tools.loli.wang',
   outDir: "docs",
-  integrations: [tailwind(),mdx({
-    extendPlugins: false,
-  })]
+  integrations: [tailwind(), mdx({
+    extendPlugins: false
+  }), vue()]
 });
